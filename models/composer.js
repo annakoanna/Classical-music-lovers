09adb3d7b0d218103f65e9d962c181493d65c0e4
annakoanna <anna.231991k@gmail.com>
2022-06-15 06:20:48 -0700

    changed model of composer

--- models/composer.js
+++ models/composer.js
@@ -1,18 +1,10 @@
 
  const mongoose = require('mongoose');
  const { Schema, model } = mongoose;
-const songSchema = new Schema({
-  name: { type: String, required: true },
-  audio: {type: String },
-})
-const composerSchema = new Schema({
-  name: { type: String, required: true },
-  songs: [songSchema],
-  playlists: [{type: Schema.Types.ObjectId, 
-  ref: "Playlist"}]
-});
-const Composer = model("Composer", composerSchema);
-const Song = model("Song", songSchema)
-
-module.exports = Composer;
-//module.exports = Song;
+ 
+ const composerSchema = new Schema({
+   name: { type: String, required: true }
+ });
+ const Composer = model("Composer", composerSchema);
+ 
+ module.exports = Composer;
