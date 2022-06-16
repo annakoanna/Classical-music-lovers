53d9bd431afd32d8737a3f409ff67728d80e645b
annakoanna <anna.231991k@gmail.com>
2022-06-16 11:33:51 -0700

    heroku dep

--- models/connection.js
+++ models/connection.js
@@ -4,18 +4,28 @@ require("dotenv").config(); // Load ENV V
 const mongoose = require("mongoose");
 
 // Setup inputs for our connect function
-const DATABASE_URL = process.env.DATABASE_URL;
+const MONGODB_URI = process.env.MONGODB_URI;
 const CONFIG = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 };
 
 // Establish Connection
-mongoose.connect(DATABASE_URL, CONFIG);// Events for when connection opens/disconnects/errors
-mongoose.connection
-    .on("open", () => console.log("Connected to Mongoose"))
-    .on("close", () => console.log("Disconnected from Mongoose"))
-    .on("error", (error) => console.log(error));
+mongoose.connect(MONGODB_URI, CONFIG);// Events for when connection opens/disconnects/errors
+// mongoose.connection
+//     .on("open", () => console.log("Connected to Mongoose"))
+//     .on("close", () => console.log("Disconnected from Mongoose"))
+//     .on("error", (error) => console.log(error));
 
+
+
+
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${mongoose.connection.host}:${mongoose.connection.port}`);
+  });
+  
+  mongoose.connection.on("error", (err) => {
+    console.log("Could not connect to MongoDB!", err);
+  });
     //export the connection
 module.exports = mongoose
