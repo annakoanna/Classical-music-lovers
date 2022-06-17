e3d7674df695690f9449c9b97fbe833cd5585921
annakoanna <anna.231991k@gmail.com>
2022-06-17 00:03:00 -0700

    update database

--- data/composers.js
+++ data/composers.js
@@ -3,16 +3,20 @@ const composersArr = [
         name: "Wolfgang Amadeus Mozart",
         songs: [
             { name: "Overture to the marriage of Figaro", audio: "audio/Overture-to-The-marriage-of-Figaro-K.-492.mp3" },
-            { name: "Alla Turca", audio: "audio/Alla-Turca.mp3" }
+            { name: "Alla Turca", audio: "audio/Alla-Turca.mp3" },{ name: "Sonata No 13", audio: "audio/Brendan_Kinsella_-_Mozart_-_Sonata_No_13_In_B_Flat_Major_K333_-_I_Allegro.mp3" },
+            { name: "Sonata No 13, part 3", audio: "audio/Brendan_Kinsella_-_Mozart_-_Piano_Sonata_in_B-flat_major_III_Allegretto_Grazioso.mp3" }
         ]
     },
+    { name: "Franz Schubert" , songs:[{ name: "Fantasy in C major", audio: "audio/Daniel_Blanch_-_Franz_Schubert_-_Fantasy_in_C_major_D760_op15_Wanderer_-_II_Adagio.mp3" }, { name: "Fantasy in C major", audio: "public/audio/Daniel_Blanch_-_Franz_Schubert_-__Fantasy_in_C_major_D760_op15_Wanderer_-_I_Allegro_con_fuoco_ma_non_troppo.mp3" }]},
     { name: "Franz List" },
+
     { name: "Pyotr Ilyich Tchaikovsky" },
     {
-        name: "Frédéric Chopin", songs:[{ name: "Polonaise in A Major", audio: "audio/Polonaise in A Major Op 40 No 1.mp3" }, { name: "Etude in G flat Major", audio: "audio/Etude in G flat Major Op 10 No 5.mp3" }, { name: "Polonaise in A Major", audio: "audio/Polonaise in A Major Op 40 No 1.mp3" }, { name: "Etude in C  Minor", audio: "audio/Etude in C Minor Op 10 No 12.mp3" }]
+        name: "Frédéric Chopin", songs:[{ name: "Polonaise in A Major", audio: "audio/Polonaise in A Major Op 40 No 1.mp3" }, { name: "Etude in G flat Major", audio: "audio/Etude in G flat Major Op 10 No 5.mp3" }, { name: "Waltz in C # Major", audio: "audio/Waltz in CSharp Minor Op 64 No 2.mp3" }, { name: "Etude in C  Minor", audio: "audio/Etude in C Minor Op 10 No 12.mp3" }, { name: "Fantaisie-Impromptu", audio: "audio/daily_download_20180817_128.mp3" }, { name: "Etude in C  Minor", audio: "audio/Etude in C Minor Op 10 No 12.mp3" }, { name: "Nocturnr No 1", audio: "audio/daily_download_20170106_128.mp3" }]
     },
     { name: "Claude Debussy" },
-    { name: "Ludwig van Beethoven" , songs:[{ name: "Sonata in F Minor part 1", audio: "audio/Daniel_Veesey_-_01_-_Sonata_No_1_in_F_Minor_Op_2_No_1_-_I_Allegro.mp3" }, { name: "Sonata in F  Minor part 4", audio: "audio/Daniel_Veesey_-_04_-_Sonata_No_1_in_F_Minor_Op_2_No_1_-_IV_Prestissimo.mp3" }]},
+    { name: "Ludwig van Beethoven" , songs:[{ name: "Sonata in F Minor, part 1", audio: "audio/Daniel_Veesey_-_01_-_Sonata_No_1_in_F_Minor_Op_2_No_1_-_I_Allegro.mp3" }, { name: "Sonata in F  Minor, part 4", audio: "audio/Daniel_Veesey_-_04_-_Sonata_No_1_in_F_Minor_Op_2_No_1_-_IV_Prestissimo.mp3" }]},
+    { name: "Johann Sebastian Bach" , songs:[{ name: "Orchestral Suite", audio: "audio/Bach-Orchestral-Suite-no.-3-in-D-major-BWV-1068-Download-free-sheet-music.mp3" }, { name: "Prelude and Fugue No 8", audio: "audio/Carlos_Gardels_-_01_-_Bach_-_Book_I_Prelude_and_Fugue_No_8_in_E_Flat_Minor_BWV_853_Prelude.mp3" }]},
     { name: "Robert Schumann" },
     { name: "Camille Saint-Saëns " },
     { name: "Igor Stravinsky " },
